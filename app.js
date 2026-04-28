373fce1dce3d4846d2093b7ad6ef2e9664dc4691
ShotaSasaki-HU <invisiblefrontline@gmail.com>
2026-04-29 01:14:14 +0900

    update: 非同期処理の同期的実行による音声の連結

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 const playButton = document.getElementById('playButton');
 
-playButton.addEventListener('click', () => {
-    const now = new Date();
-    const currentHour = now.getHours(); // 0-23
-
-    const hourFileName = `./voice_clips/v_${currentHour}.mp3`;
-    console.log('Now playing:', hourFileName)
-
-    const hourSound = new Howl({
-        src: [hourFileName]
+// 音を1つ鳴らして終わるまで待つ関数．Promiseのインスタンスを返す．
+function playSignalSound(fileName) {
+    // Promiseは，非同期処理の状態や結果を表現するオブジェクト．
+    return new Promise((resolve) => {
+        const sound = new Howl({
+            src: [fileName],
+            onend: () => { resolve(); } // 音が最後まで再生された時の処理
+        });
+        sound.play();
     });
+}
 
-    hourSound.play();
+// asyncで非同期関数にする．awaitが使えるようになる．
+playButton.addEventListener('click', async () => {
+    const now = new Date();
+    const currentHour = now.getHours();
+
+    const clips_path = './voice_clips/'
+    const playlist = [
+        clips_path + 'v_at_the_tone.mp3',
+        clips_path + `v_${currentHour}.mp3`,
+        clips_path + 'v_hours.mp3'
+    ];
+
+    console.log('Playing sequence:', playlist);
+
+    for (const fileName of playlist) {
+        // awaitは，Promiseオブジェクトが値を返すのを待つ演算子．
+        // Promiseオブジェクトが値を返すまで，この関数のみ一時停止する．他のUI操作などは止まらないよ．
+        await playSignalSound(fileName);
+    }
+
+    console.log('Announcement finished.');
 });
